7fcca0cf6d6b91b614f4d443137afa901a43a0df
Nikos Fytros <nikos.fytros@outlook.com>
2019-05-22 13:01:13 +0200

    removed vars and use consistent styles for strings

--- lib/imports/servue.vue
+++ lib/imports/servue.vue
@@ -1,87 +1,102 @@
 <template>
-    <div id="app">
-        <slot name="content"></slot>
-    </div>
+  <div id="app">
+    <slot name="content"></slot>
+  </div>
 </template>
 <script>
-const unaryTags = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 
-'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr']
+const unaryTags = [
+  "area",
+  "base",
+  "br",
+  "col",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "keygen",
+  "link",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr"
+];
 
-function renderStartTag(VNode){
-    let html = `<${VNode.tag}`
+function renderStartTag(VNode) {
+  let html = `<${VNode.tag}`;
 
-    if(VNode.data){
-        if(VNode.data.attrs){
-            let attr = VNode.data.attrs
-            for(let name in attr){
-                if(attr[name] === ''){
-                    html += ` ${name}`
-                }else{
-                    html += ` ${name}="${attr[name]}"`
-                }
-            }
+  if (VNode.data) {
+    if (VNode.data.attrs) {
+      let attr = VNode.data.attrs;
+      for (let name in attr) {
+        if (attr[name] === "") {
+          html += ` ${name}`;
+        } else {
+          html += ` ${name}="${attr[name]}"`;
         }
+      }
     }
+  }
 
-    return html + '>'
+  return html + ">";
 }
 
-function isUnaryTag(VNode){
-    return (unaryTags.indexOf(VNode.tag) > -1)
+function isUnaryTag(VNode) {
+  return unaryTags.indexOf(VNode.tag) > -1;
 }
 
-function getFullTag(VNode){
-    if(!VNode.tag) return VNode.text
+function getFullTag(VNode) {
+  if (!VNode.tag) return VNode.text;
 
-    let html = renderStartTag(VNode)
+  let html = renderStartTag(VNode);
 
-    if(VNode.children){
-        html += getChildren(VNode)
-    }
-    if(!isUnaryTag(VNode)){
-        html += `</${VNode.tag}>`
-    }
-    return html
+  if (VNode.children) {
+    html += getChildren(VNode);
+  }
+  if (!isUnaryTag(VNode)) {
+    html += `</${VNode.tag}>`;
+  }
+  return html;
 }
 
-function getChildren(VNode){
-    let html = ''
-    for(let i in VNode.children){
-        let child = VNode.children[i]
-        html += getFullTag(child)
-    }
-    return html
+function getChildren(VNode) {
+  let html = "";
+  for (let i in VNode.children) {
+    let child = VNode.children[i];
+    html += getFullTag(child);
+  }
+  return html;
 }
 
 let mixin = {
-    created(){
-        if(this.$isServer){
-            let VNodes = this.$slots.head
-            let renderedHead = ""
+  created() {
+    if (this.$isServer) {
+      let VNodes = this.$slots.head;
+      let renderedHead = "";
 
-            for(let i in VNodes){
-                let VNode = VNodes[i]
-                renderedHead += getFullTag(VNode)
-            }
+      for (let i in VNodes) {
+        let VNode = VNodes[i];
+        renderedHead += getFullTag(VNode);
+      }
 
-            this.$ssrContext.head = renderedHead
-        }
+      this.$ssrContext.head = renderedHead;
     }
-}
+  }
+};
 
 export default {
-    created(){
-        if(this.$isServer){
-            let VNodes = this.$slots.head
-            let renderedHead = ""
+  created() {
+    if (this.$isServer) {
+      let VNodes = this.$slots.head;
+      let renderedHead = "";
 
-            for(let i in VNodes){
-                let VNode = VNodes[i]
-                renderedHead += getFullTag(VNode)
-            }
+      for (let i in VNodes) {
+        let VNode = VNodes[i];
+        renderedHead += getFullTag(VNode);
+      }
 
-            this.$ssrContext.head = renderedHead
-        }
+      this.$ssrContext.head = renderedHead;
     }
-}
+  }
+};
 </script>
